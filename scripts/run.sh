0696a818374ddf7c4ad415e7920d6a420a73a800
hrx20000209 <rhuangbj@connect.ust.hk>
2026-04-27 22:17:46 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,13 +2,13 @@ lerobot-record \
   --robot.type=so101_follower \
   --robot.port=/dev/ttyACM0 \
   --robot.id=follower_arm \
-  --robot.cameras="{ base_0_rgb: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
+  --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, wrist: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
   --dataset.repo_id=hrx2000/eval_pi0 \
   --dataset.single_task="Grab the blue cube" \
-  --policy.type=hrx2000/Lerobot_SmolVLA_Blue_Block \
+  --policy.path=/home/hrx/Projects/models/act/ \
   --teleop.type=so101_leader \
   --dataset.num_episodes=1 \
-  --dataset.fps=30 \
+  --dataset.fps=4 \
   --dataset.episode_time_s=600 \
   --dataset.push_to_hub=false \
   --display_data=true \
@@ -20,15 +20,33 @@ lerobot-record \
 # remote
 python -m lerobot.async_inference.robot_client \
   --server_address=100.127.53.101:8080 \
-  --robot.type=so100_follower \
+  --robot.type=so101_follower \
   --robot.port=/dev/ttyACM0 \
   --robot.id=follower_so100 \
   --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, wrist: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
   --task="Grab the blue block" \
-  --policy_type=fast_wam \
-  --pretrained_name_or_path=/home/rxhuang/Projects/lerobot/output_lerobot_train/grab_blue_block/fast_wam/checkpoints/last/pretrained_model \
+  --policy_type=act \
+  --pretrained_name_or_path=/home/hrx/Projects/models/pi05/ \
   --policy_device=cuda \
   --actions_per_chunk=50 \
   --chunk_size_threshold=0.5 \
   --aggregate_fn_name=weighted_average \
   --debug_visualize_queue_size=True
+
+
+# remote localhost
+python -m lerobot.async_inference.robot_client \
+  --server_address=127.0.0.1:8080 \
+  --robot.type=so101_follower \
+  --robot.port=/dev/ttyACM0 \
+  --robot.id=follower_arm \
+  --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, wrist: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
+  --task="Grab the blue block" \
+  --policy_type=act \
+  --pretrained_name_or_path=/home/hrx/Projects/models/act/ \
+  --policy_device=cuda \
+  --actions_per_chunk=50 \
+  --chunk_size_threshold=0.8 \
+  --aggregate_fn_name=weighted_average \
+  --debug_visualize_queue_size=True 
+
