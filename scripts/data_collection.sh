0917a12acda833b7215f1d393979b8f3d177bba3
hrx200000209 <rhuangbj@connect.ust.hk>
2026-04-28 19:49:08 +0800

    update

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -1,11 +1,11 @@
 # data collection
 lerobot-record \
     --robot.type=so101_follower \
-    --robot.port=/dev/ttyACM0 \
+    --robot.port=/dev/ttyACM1 \
     --robot.id=follower_arm \
-    --robot.cameras="{ front: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, right: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
+    --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, right: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
     --teleop.type=so101_leader \
-    --teleop.port=/dev/ttyACM1 \
+    --teleop.port=/dev/ttyACM0 \
     --teleop.id=leader_arm \
     --display_data=true \
     --dataset.repo_id=hrx2000/lerobot_grab_blue_cube_2 \
--- src/lerobot/robots/so_follower/so_follower.py
+++ src/lerobot/robots/so_follower/so_follower.py
@@ -178,7 +178,7 @@ class SOFollower(Robot):
     def get_observation(self) -> RobotObservation:
         # Read arm position
         start = time.perf_counter()
-        obs_dict = self.bus.sync_read("Present_Position")
+        obs_dict = self.bus.sync_read("Present_Position", num_retry=3)
         obs_dict = {f"{motor}.pos": val for motor, val in obs_dict.items()}
         dt_ms = (time.perf_counter() - start) * 1e3
         logger.debug(f"{self} read state: {dt_ms:.1f}ms")
