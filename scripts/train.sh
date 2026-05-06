04fb331feccd70e3dd43d775f901b205e2850a79
hrx200000209 <rhuangbj@connect.ust.hk>
2026-05-06 23:22:01 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,24 +1,25 @@
-CUDA_VISIBLE_DEVICES=1 lerobot-train \
-  --policy.type=act \
-  --dataset.repo_id=hrx2000/lerobot_grab_blue_cube \
-  --dataset.root=/data/hf_cache/hub/datasets--hrx2000--lerobot_grab_blue_cube/snapshots/2bec0c4eddf2c913509ba414f76c68fa0c918ab4 \
+CUDA_VISIBLE_DEVICES=7 lerobot-train \
+  --policy.type=smolvla \
+  --dataset.repo_id=hrx2000/lerobot_grab_blue_cube_2 \
+  --dataset.root=/data/rxhuang/lerobot_grab_blue_cube/ \
   --dataset.revision=v0.1.0 \
   --dataset.streaming=false \
-  --output_dir=output_lerobot_train/grab_blue_block/act \
+  --output_dir=output_lerobot_train/grab_blue_block/smolvla \
   --job_name=grab_blue_cube \
   --policy.device=cuda \
   --wandb.enable=false \
   --wandb.project=Lerobot_Blue_Cube \
   --policy.push_to_hub=false \
   --steps=10000 \
-  --batch_size=16
+  --batch_size=16 \
+  --save_freq=2000 \
 
 
 # train pi05
 lerobot-train \
   --policy.type=pi05 \
-  --dataset.repo_id=hrx2000/lerobot_grab_blue_cube \
-  --dataset.root=/data/hf_cache/hub/datasets--hrx2000--lerobot_grab_blue_cube/snapshots/2bec0c4eddf2c913509ba414f76c68fa0c918ab4 \
+  --dataset.repo_id=hrx2000/lerobot_grab_blue_cube_2 \
+  --dataset.root=/data/rxhuang/lerobot_grab_blue_cube \
   --dataset.revision=v0.1.0 \
   --dataset.streaming=false \
   --output_dir=output_lerobot_train/grab_blue_block/pi05 \
@@ -32,5 +33,26 @@ lerobot-train \
   --wandb.project=Lerobot_Blue_Cube \
   --policy.push_to_hub=false \
   --steps=30000 \
+  --batch_size=1
+
+
+lerobot-train \
+  --policy.type=fast_wam \
+  --dataset.repo_id=hrx2000/lerobot_grab_blue_cube_2 \
+  --dataset.root=/data/rxhuang/lerobot_grab_blue_cube \
+  --dataset.revision=v0.1.0 \
+  --dataset.streaming=false \
+  --output_dir=output_lerobot_train/grab_blue_block/fast_wam \
+  --job_name=grab_blue_cube \
+  --policy.device=cuda \
+  --policy.dtype=bfloat16 \
+  --policy.gradient_checkpointing=true \
+  --policy.freeze_vision_encoder=true \
+  --policy.train_expert_only=true \
+  --wandb.enable=false \
+  --wandb.project=Lerobot_Blue_Cube \
+  --policy.push_to_hub=false \
+  --steps=30000 \
+  --save_freq=5000 \
   --batch_size=1
   
