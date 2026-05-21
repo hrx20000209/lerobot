4e06a98e47e1938bd43fc05220cf4351eac92af4
hrx200000209 <rhuangbj@connect.ust.hk>
2026-05-21 21:26:08 +0800

    update

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -3,16 +3,16 @@ lerobot-record \
     --robot.type=so101_follower \
     --robot.port=/dev/ttyACM0 \
     --robot.id=follower_arm \
-    --robot.cameras="{ front: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, right: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
+    --robot.cameras="{ front: {type: opencv, index_or_path: 4, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, right: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, wrist: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
     --teleop.type=so101_leader \
     --teleop.port=/dev/ttyACM1 \
     --teleop.id=leader_arm \
     --display_data=true \
-    --dataset.repo_id=hrx2000/Move_Block \
-    --dataset.num_episodes=50 \
-    --dataset.single_task="Move the cube inside the box." \
+    --dataset.repo_id=hrx2000/Blue_Cube \
+    --dataset.num_episodes=100 \
+    --dataset.single_task="Pick up the blue cube into the box." \
     --dataset.push_to_hub=true \
-    --dataset.episode_time_s=30 \
+    --dataset.episode_time_s=20 \
     --dataset.reset_time_s=3
 
 
@@ -20,11 +20,11 @@ lerobot-record \
     --robot.type=so101_follower \
     --robot.port=/dev/ttyACM0 \
     --robot.id=follower_arm \
-    --robot.cameras="{ front: {type: opencv, index_or_path: 4, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, right: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
+    --robot.cameras="{ front: {type: opencv, index_or_path: 4, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, right: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, wrist: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
     --display_data=false \
     --dataset.repo_id=hrx2000/eval_pi0 \
     --dataset.num_episodes=50 \
-    --dataset.single_task="Move the cube inside the box." \
+    --dataset.single_task="Pick up the blue cube into the box." \
     --dataset.push_to_hub=true \
     --dataset.episode_time_s=30 \
     --dataset.reset_time_s=3 \
