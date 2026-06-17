4d429fcc93ddbd8ac71c7d990a29aa295ef72751
hrx200000209 <rhuangbj@connect.ust.hk>
2026-06-17 16:28:29 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -41,9 +41,7 @@ python -m lerobot.async_inference.robot_client \
   --robot.type=so101_follower \
   --robot.port=/dev/ttyACM1 \
   --robot.id=follower_arm \
-  --robot.cameras='{ front: {type: opencv, index_or_path: /dev/v4l/by-path/platform-a80aa10000.usb-usb-0:4.2.4:1.0-video-index0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, wrist: {type:
-    opencv, index_or_path: /dev/v4l/by-path/platform-a80aa10000.usb-usb-0:4.2.2:1.0-video-index0, width: 640, height: 480, fps: 30}, right: {type: opencv, index_or_path: /dev/v4l/by-path/platform-a80aa10000.usb-usb-0:4.2.1:1.0-video-index0, width: 640, height: 480,
-    fps: 30, fourcc: "MJPG"}}' \
+  --robot.cameras="{ front: {type: opencv, index_or_path: 4, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, right: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30, fourcc: "MJPG"}, wrist: {type: opencv, index_or_path: 2, width: 640, height: 480, fps: 30, fourcc: "MJPG"}}" \
   --task="go to red cube. take the red cube. go to box. put the red cube in box." \
   --policy_type=pi05 \
   --pretrained_name_or_path=/home/hrx/Projects/models/three_cubes_1/pi05_lora_expert/ \
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import cv2
 
-cap = cv2.VideoCapture(4, cv2.CAP_V4L2)
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FPS, 15)
+cap.set(cv2.CAP_PROP_FPS, 30)
 
 print("opened:", cap.isOpened())
 print("fourcc:", int(cap.get(cv2.CAP_PROP_FOURCC)))
